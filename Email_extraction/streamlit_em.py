df9d2bd6be93e42d5f0d6cb11acc484d0c45433f
Akshat Shrivastava <94971651+akshatsrv@users.noreply.github.com>
2024-05-03 11:15:23 +0530

    Update streamlit_em.py

--- Email_extraction/streamlit_em.py
+++ Email_extraction/streamlit_em.py
@@ -16,7 +16,7 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.os_manager import ChromeType
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromiumService
+from selenium.webdriver.chrome.service import Service as ChromeService
 
 
 
